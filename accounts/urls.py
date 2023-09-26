efbbb021406ee57675df7130c65113ee353c6670
Hussaini Usman <hussainmaina27@gmail.com>
2023-09-26 16:18:29 +0100

    add password reset confirm path to accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from .views import GoogleLogin, FacebookLogin
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path(
+        "password/reset/confirm/<uid>/<token>/",
+        TemplateView.as_view(template_name="password_reset_confirm.html"),
+        name="password_reset_confirm",
+    ),
     path("", include("dj_rest_auth.urls")),
     path("google/", GoogleLogin.as_view(), name="google_login"),
     path("facebook/", FacebookLogin.as_view(), name="fb_login"),
