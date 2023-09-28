030fd05cf1419a230d02d195fff7f42bb883b7ee
Hussaini Usman <hussainmaina27@gmail.com>
2023-09-28 21:29:03 +0100

    update static files settings

--- core/settings.py
+++ core/settings.py
@@ -156,11 +156,17 @@ USE_TZ = True
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
